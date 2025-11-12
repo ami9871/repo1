811db076837b3e9a7b9f83cbc62d8f37d40b49a1
ami9871 <amitakatiyar1994@gmail.com>
2025-11-12 11:29:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <header className="App-header">
+        <h1>Welcome to MyApp</h1>
+        <p>Now with a reusable horizontal Navbar 🎉</p>
+      </header>
+    </>
+  );
+}
+
+export default App;
